246f6a36f17ef4201711c181d1125d52f71b8521
guilherme <guitimoteo@hotmail.com>
2019-06-21 14:07:03 -0300

    adicionar interceptor ao registro.

---
+++ src/main/java/br/com/multitenant/config/WebMVCConfigureImpl.java
@@ -0,0 +1,18 @@
+package br.com.multitenant.config;
+
+import br.com.multitenant.interceptor.TenantInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfigureImpl implements WebMvcConfigurer {
+    @Autowired
+    private TenantInterceptor tenantInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tenantInterceptor);
+    }
+}
--- src/main/java/br/com/multitenant/interceptor/TenantInterceptor.java
+++ src/main/java/br/com/multitenant/interceptor/TenantInterceptor.java
@@ -1,5 +1,6 @@
 package br.com.multitenant.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -8,13 +9,18 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Intercept request in search of X-TENANT header.
+ */
+@Slf4j
 @Component
 public class TenantInterceptor implements HandlerInterceptor {
     private static final String TENANT_HEADER = "X-TENANT";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        RequestContextHolder.getRequestAttributes().setAttribute("tenantId", Long.getLong(request.getHeader(TENANT_HEADER)), RequestAttributes.SCOPE_REQUEST);
+        log.info("Interceptando a requisicao: {}", request.getHeader(TENANT_HEADER));
+        RequestContextHolder.getRequestAttributes().setAttribute("tenantId", request.getHeader(TENANT_HEADER), RequestAttributes.SCOPE_REQUEST);
         return true;
     }
 }
