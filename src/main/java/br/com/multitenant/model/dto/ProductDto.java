81a75e13e8233e39b3e90eddd948f4876f0bcc2b
guilherme <guitimoteo@hotmail.com>
2019-06-25 09:08:02 -0300

    corrigir a falta de construtora e adicionar valor a ser atualizado na classe

--- src/main/java/br/com/multitenant/model/dto/ProductDto.java
+++ src/main/java/br/com/multitenant/model/dto/ProductDto.java
@@ -2,11 +2,15 @@ package br.com.multitenant.model.dto;
 
 import br.com.multitenant.model.entity.Product;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class ProductDto {
     private long sku;
+    private float value;
     public ProductDto(Product product) {
         this.sku = product.getSku();
+        this.value = product.getValue();
     }
 }
