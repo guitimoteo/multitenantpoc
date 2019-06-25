eb1a5d3ae59a1c5b2268553f218c43827e3836a7
guilherme <guitimoteo@hotmail.com>
2019-06-25 09:05:13 -0300

    limpar codigo desnecessario.

--- src/main/java/br/com/multitenant/config/MongoDbConfiguration.java
+++ src/main/java/br/com/multitenant/config/MongoDbConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.context.request.RequestContextListener;
 
 import java.net.UnknownHostException;
 
--- src/main/java/br/com/multitenant/repository/CatalogRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.multitenant.repository;
-
-import br.com.multitenant.model.entity.Catalog;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-public interface CatalogRepository extends CrudRepository<Catalog, Long> {
-}
