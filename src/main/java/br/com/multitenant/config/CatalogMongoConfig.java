2fe2cff03a32be782cd73a9895a1235507527140
guilherme <guitimoteo@hotmail.com>
2019-06-25 09:03:13 -0300

    retirar a repository do catalago.

--- src/main/java/br/com/multitenant/config/CatalogMongoConfig.java
+++ src/main/java/br/com/multitenant/config/CatalogMongoConfig.java
@@ -1,20 +1,15 @@
 package br.com.multitenant.config;
 
-import br.com.multitenant.repository.CatalogRepository;
 import com.mongodb.MongoClientURI;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.net.UnknownHostException;
 
 @Configuration
-@EnableMongoRepositories(basePackageClasses = CatalogRepository.class,
-                         mongoTemplateRef   = "catalogMongoTemplate")
 public class CatalogMongoConfig {
     @Primary
     @Bean(name = "catalogMongoTemplate")
