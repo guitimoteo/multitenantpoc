69fd5bca2e9dd563f7920c6f9c05b56edb60f58f
guitimoteo <guitimoteo@yahoo.com.br>
2019-06-25 23:42:24 -0300

    adicionar metodo de conferencia de produto

--- src/main/java/br/com/multitenant/repository/ProductRepository.java
+++ src/main/java/br/com/multitenant/repository/ProductRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository("catalog")
 public interface ProductRepository extends CrudRepository<Product, Long> {
     void deleteBySku(Long sku);
+    Product findBySku(Long sku);
+    boolean existsBySku(long sku);
 }
