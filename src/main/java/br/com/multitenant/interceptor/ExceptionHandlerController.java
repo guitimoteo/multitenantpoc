6224bdfe882e60261e8041eedeb6929ebee450a5
guitimoteo <guitimoteo@yahoo.com.br>
2019-06-25 23:43:45 -0300

    adicao de mais um status de erro(desenvolvimento em andamento)

--- src/main/java/br/com/multitenant/interceptor/ExceptionHandlerController.java
+++ src/main/java/br/com/multitenant/interceptor/ExceptionHandlerController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -11,6 +12,11 @@ public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {IllegalArgumentException.class})
     private ResponseEntity illegalExceptionHandler(IllegalArgumentException e, WebRequest request) throws Exception {
+        return handleException(e, request);
+    }
+
+    @ExceptionHandler(value = {ResponseStatusException.class})
+    private ResponseEntity notFoundExceptionHandler(IllegalArgumentException e, WebRequest request) throws Exception {
         return handleException(e, request);
     }
 }
